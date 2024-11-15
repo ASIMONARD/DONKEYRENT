b9e426621c91b44db4df59932dea77f49d24c6d8
Vania Goncalves <vfatimagoncalves@gmail.com>
2024-11-15 09:27:26 +0100

    create new files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Donkeyrent</title>
+</head>
+
+<body>
+    <nav>
+        <a href=""><img class="logo" src="images/AneCar.png" alt=""></a>
+        <div>
+            <a href="">Accueil</a>
+            <a href="">Toutes les voitures</a>
+            <a href="">Se connecter</a>
+        </div>
+    </nav>
+    <div class="cardAdult">
+        <div class="card">
+            <div class="voiture">
+            </div>
+            <button>Réserver</button>
+        </div>
+    </div>
+</body>
+
+</html>
