b2b151f8264ef81c87a7d8cd2bd24ba2a039b316
ASIMONARD <ar.simonard.com>
2024-11-20 14:38:41 +0100

    Modify tree structure of project

---
+++ config/constant.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_ROOT', 'D:\DONKEY-SCHOOL\Cours\PHP-MYSQL\POO\library');
+define('DB_HOST', 'mysql:dbname=donkeyrent;host=localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_PORT', '3306');  
+?>
